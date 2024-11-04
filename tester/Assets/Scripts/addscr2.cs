88e34b61f8a2dd090ceb26491cf71c4a3296a1a0
unkervur <pblzhiykjioyh@gmail.com>
2024-11-04 13:55:23 +0300

    1.2

--- tester/Assets/Scripts/addscr2.cs
+++ tester/Assets/Scripts/addscr2.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class addscr2 : MonoBehaviour
 {
+    public int y = 0;
+    public Text type;
+    public GameObject sphere;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +16,27 @@ public class addscr2 : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
+    }
+    public void button()
+    {
+        script2.r = Random.Range(0, 255);
+        script2.g = Random.Range(0, 255);
+        script2.b = Random.Range(0, 255);
+        this.GetComponent<Renderer>().material.color = new Color32((byte)script2.r, (byte)script2.g, (byte)script2.b, 3);
+
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        ++y;
+        type.text = y.ToString();
+
+        if (y == 100)
+        {
+            sphere.gameObject.SetActive(false);
+        }
     }
 }
--- tester/Assets/Scripts/script2.cs
+++ tester/Assets/Scripts/script2.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-// В этом скрипте ДЗ, заданное на 3 семинар.
+/* В этом скрипте и скрипте addscr2 ДЗ, заданное на 3 семинаре. Сфера при взаимодействии с кубом меняет его цвет, 
+ * а при y=100 сфера удаляется.*/ 
 
 public class script2 : MonoBehaviour
 {
-    public int r, g, b;
-    public int x = 0;
+    static public int r, g, b;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,16 +17,19 @@ public class script2 : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        
 
     }
 
-    public void button()
+    public void OnTriggerStay(Collider other)
     {
         r = Random.Range(0, 255);
         g = Random.Range(0, 255);
         b = Random.Range(0, 255);
+
         this.GetComponent<Renderer>().material.color = new Color32((byte)r, (byte)g, (byte)b, 3);
+
     }
 }
